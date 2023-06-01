e5cfcef06ede0fdb0d68f23833a9fb7437b16e33
hs2425 <134681784+hs2425@users.noreply.github.com>
2023-06-01 20:37:32 +0200

    Add files via upload

--- bigdataReddit.py
+++ bigdataReddit.py
@@ -4,7 +4,7 @@ import datetime
 import json
 from confluent_kafka import Producer
 from praw.models import MoreComments
-
+import time
 import pandas as pd
 
 p = Producer({'bootstrap.servers': 'localhost:9092'})
@@ -30,8 +30,8 @@ params = {'limit': 100,
           'time_filter': "hour"}
 while True:
     posts = list(subreddit.search('*', params=params))
-    if not posts:
-        break
+    #if not posts:
+    #    break
     for submission in posts:
         print(submission.title)
         post_dict = {
@@ -42,30 +42,10 @@ while True:
         }
 
         byte_like = json.dumps(post_dict).encode('utf-8')
-        p.produce('twitter_messages', byte_like)
-
-        # Hier die Kafka Nachricht
-        data.append(post_dict)
+        p.produce('reddit_messages', byte_like)
 
     params['after'] = posts[-1].fullname
     p.flush()
 
-        #'Autor': submission.author.name,
-        #'Subreddit': submission.subreddit.display_name,
-        #'Anzahl der Kommentare': submission.num_comments,
-        #'Punktzahl': submission.score
+    time.sleep(15)
 
-    # extract comments
-    # comments = post.comments.list()
-    # comment_texts = []
-    # for comment in comments:
-    #     if not isinstance(comment, MoreComments):
-    #         comment_texts.append(comment.body)
-    data.append(post_dict)
-
-# create dataframes and save them as csv-files
-df_data = pd.DataFrame(data)
-df_data.to_csv('Data.csv',sep=';',encoding='utf-8', index=False)
-
-# df_comments = pd.DataFrame(comment_texts)
-# df_comments.to_csv('Comments.csv',sep=';',encoding='utf-8', index=False)
