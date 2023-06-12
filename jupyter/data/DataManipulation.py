23d36aacf30c0544f9d8cf18377751b9c9f0875d
alphasigmaiota <alwine.schultze@outlook.de>
2023-06-12 20:27:21 +0200

    Updated DataManipulation.py

--- jupyter/data/DataManipulation.py
+++ jupyter/data/DataManipulation.py
@@ -29,18 +29,22 @@ DATA_ATTRIBUTES_FOR_CLEANUP = ["Titel", "Inhalt"]
 spark = SparkSession.builder \
     .master("local[*]") \
     .appName(APP_NAME) \
-    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1") \
+    .config("spark.jars.packages",
+            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1") \
     .getOrCreate()
+
 
 # Datenmanipulationen vor dem Speichern
 def data_manipulation(df, epoch_id):
     new_df = df
 
-    # neues Dataframe mit den Werten erstellen und zurück liefern für die Persistierung
+    # neues Dataframe mit den Werten erstellen und zurück liefern
+    # für die Persistierung
     manipulate_text_udf = F.udf(manipulate_text, F.StringType())
     new_df = new_df.withColumn("value", manipulate_text_udf(col("value")))
 
     return new_df
+
 
 # Textmanipulation für die Werte "Titel" und "Inhalt"
 def manipulate_text(value):
@@ -56,39 +60,48 @@ def manipulate_text(value):
         data[data_attr] = remove_stopwords(text)
     return json.dumps(data)
 
+
 # Entfernen unnötiger Leerzeichen
 def remove_whitespace(text):
     text = text.replace('\r', ' ')
     return text.replace('\n', ' ')
 
+
 # Entfernen von Sonderzeichen
 def remove_special_characters(text):
     return re.sub('[^a-zA-z0-9\s]', '', text)
+
 
 # Entfernen der Links
 def remove_links(text):
     pattern = r"http\S+|www\S+"
     return re.sub(pattern, "", text)
 
+
 # Alles in Kleinschreibung umwandeln
 def lower_case(text):
     return text.lower()
 
-# ggf. tokenizen, aber nur wenn topic modelling etc. gemacht wird, nicht wenn Q&A bestehen bleibt
+
+# ggf. tokenizen, aber nur wenn topic modelling etc. gemacht wird,
+# nicht wenn Q&A bestehen bleibt
 def tokenize(text):
     wordTokens = nltk.word_tokenize(text)
     return " ".join([token for token in wordTokens])
+
 
 # Stoppwörter entferenn
 def remove_stopwords(text):
     wordTokens = nltk.word_tokenize(text)
     return " ".join([word for word in wordTokens if word not in stopWords])
 
-# Methode zum Schreiben der gelesenen Daten in das Hadoop-Verzeichnis "redidits"
+
+# Methode zum Schreiben der gelesenen Daten in die Hadoop-Verzeichnisse
+# "redidits" und "reddits_manipulated"
 def write_to_hadoop(df, epoch_id):
     df_raw = df
     df_mani = data_manipulation(df, epoch_id)
-    
+
     # Datumsstring für heutiges Datum (Start Datum des Jobs)
     date_str = datetime.datetime.now().strftime("%Y-%m-%d")
     # Speicherorte für raw Data und manipulierte Daten
@@ -99,19 +112,21 @@ def write_to_hadoop(df, epoch_id):
     df_raw.write.csv(fn_raw, mode="append")
     df_mani.write.csv(fn_mani, mode="append")
 
-# Verbindung zum Kafka Topic für das Lesen der Daten:
-df = spark \
-    .readStream \
-    .format("kafka") \
-    .option("kafkaConsumer.pollTimeoutMs", 60*60*1000) \
-    .option("kafka.bootstrap.servers", "kafka-broker:9092") \
-    .option("startingOffsets", "earliest") \
-    .option("subscribe", TOPIC) \
-    .load()
 
-# Konvertierung der Ausagabe aus Kafka in einen String
-df = df.selectExpr("CAST(value AS STRING)")
-# Verarbeitungsquery aufbauen und für jeden eingelesenen "batch" Kafka-Messages die Methode "write_to_hadoop" aufrufen
-query = df.writeStream.foreachBatch(write_to_hadoop).start()
-# Solange auf Kafka hören, bis der Abbruch kommt.
-query.awaitTermination()
+if __name__ == "__main__":
+    # Verbindung zum Kafka Topic für das Lesen der Daten:
+    df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafkaConsumer.pollTimeoutMs", 60*60*1000) \
+        .option("kafka.bootstrap.servers", "kafka-broker:9092") \
+        .option("startingOffsets", "earliest") \
+        .option("subscribe", TOPIC) \
+        .load()
+
+    # Konvertierung der Ausagabe aus Kafka in einen String
+    df = df.selectExpr("CAST(value AS STRING)")
+    # Verarbeitungsquery aufbauen und für jeden eingelesenen "batch" Kafka-Messages die Methode "write_to_hadoop" aufrufen
+    query = df.writeStream.foreachBatch(write_to_hadoop).start()
+    # Solange auf Kafka hören, bis der Abbruch kommt.
+    query.awaitTermination()
